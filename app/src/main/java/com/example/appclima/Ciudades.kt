75f94825045a153d40553e3c82355d186bacbe20
HeynerPat <72895524+HeynerPat@users.noreply.github.com>
2022-02-20 19:02:49 -0500

    Intent Añadido

--- app/src/main/java/com/example/appclima/Ciudades.kt
+++ app/src/main/java/com/example/appclima/Ciudades.kt
@@ -1,11 +1,27 @@
 package com.example.appclima
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class Ciudades : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ciudades)
+        val bFCP = findViewById<Button>(R.id.bFCP)
+        val bBerlin = findViewById<Button>(R.id.bBerlin)
+
+        bFCP.setOnClickListener(View.OnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        })
+
+        bBerlin.setOnClickListener(View.OnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
